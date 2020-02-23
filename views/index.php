d4c9067a43d8fa959a1ca0af2d8277dbe8489221
Matthew Brown <github@muglug.com>
2020-02-23 12:22:37 -0500

    Bump version

--- views/index.php
+++ views/index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Psalm - a static analysis tool for PHP</title>
-<script src="/assets/js/codemirror.js"></script>
+<script src="/assets/js/codemirror.js?1"></script>
 <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/751592/7707372/css/fonts.css" />
 <meta property="og:image" content="psalm_preview.png" />
 <link rel="stylesheet" href="/assets/css/site.css?1">
