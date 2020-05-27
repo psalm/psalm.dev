9fe679b4e265cda7d79daece5cc0dbac978d8aff
Matthew Brown <github@muglug.com>
2020-05-27 17:25:40 -0400

    Update contribute.php

--- views/contribute.php
+++ views/contribute.php
@@ -24,7 +24,7 @@ $html = PsalmDotOrg\ArticleRepository::convertMarkdownToHtml($contribution_markd
 <h1><?= PsalmDotOrg\AltHeadingParser::preventOrphans('Contribute to Psalm!') ?></h1>
 <div class="contribute_intro">
   <img src="https://psalm.dev/assets/images/beardy_me.jpg" alt="" width="75" height="100" style="align: right; padding: 0 20px 10px 10px; float: left;">
-  <p>Hi, I'm Matt, the creator of Psalm. I work at Vimeo, and part of my job there involves maintaining Psalm (I spend a lot of non-work time on Psalm as well).
+  <p>Hi, I'm Matt, the creator of Psalm. I work at Vimeo, and part of my job there involves maintaining Psalm (I spend a lot of my spare time on Psalm as well).
   </p>
 </div>
 <?= $html ?>
