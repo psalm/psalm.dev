fae4db3ac31fc684434f1ad2455670e57380514c
Matthew Brown <github@muglug.com>
2020-05-27 12:24:12 -0400

    Reposition image

--- views/contribute.php
+++ views/contribute.php
@@ -23,9 +23,8 @@ $html = PsalmDotOrg\ArticleRepository::convertMarkdownToHtml($contribution_markd
 <div class="post">
 <h1><?= PsalmDotOrg\AltHeadingParser::preventOrphans('Contribute to Psalm!') ?></h1>
 <div class="contribute_intro">
-  <p><img src="https://psalm.dev/assets/images/beardy_me.jpg" alt="" width="150" height="200" style="align: right;padding-left: 20px;">
-    Hi, I'm Matt, the creator of Psalm.
-    I work at Vimeo, and part of my job there involves maintaining Psalm (I spend a lot of non-work time on Psalm as well).
+  <img src="https://psalm.dev/assets/images/beardy_me.jpg" alt="" width="150" height="200" style="align: right; padding-left: 20px;float: right;">
+  <p>Hi, I'm Matt, the creator of Psalm. I work at Vimeo, and part of my job there involves maintaining Psalm (I spend a lot of non-work time on Psalm as well).
   </p>
 </div>
 <?= $html ?>
