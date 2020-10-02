28f39e0ee9c81597e6a1c30847742806024c80ab
Matt Brown <github@muglug.com>
2020-10-02 11:26:48 -0400

    Bold headers

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	mode: 'production',
-	entry: ['./assets/sass/site.scss', './assets/sass/mkdocs_extra.scss', './assets/sass/mkdocs_material_extra.scss'],
+	entry: ['./assets/sass/site.scss', './assets/sass/mkdocs_material_extra.scss'],
 	output: {
 		path: path.resolve(__dirname, 'assets'),
 	},
