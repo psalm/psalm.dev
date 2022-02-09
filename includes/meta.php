c651775151425419dc581ff45653534a26de2971
Matthew Brown <github@muglug.com>
2022-02-09 11:17:24 -0500

    Fix footer position in Firefox

--- includes/meta.php
+++ includes/meta.php
@@ -1,6 +1,6 @@
 <script src="/assets/js/codemirror.js?1"></script>
 <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/751592/7707372/css/fonts.css" />
 <meta property="og:image" content="psalm_preview.png" />
-<link rel="stylesheet" href="/assets/css/site.css?19">
+<link rel="stylesheet" href="/assets/css/site.css?20">
 <link rel="icon" type="image/png" href="/favicon.png">
 <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
