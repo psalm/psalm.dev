57f099275433cf87f6bb39fd1ab1e026e1914a52
Matthew Brown <github@muglug.com>
2022-03-26 00:41:03 +0000

    Remove cloud typography fonts

--- includes/meta.php
+++ includes/meta.php
@@ -1,5 +1,4 @@
 <script src="/assets/js/codemirror.js?1"></script>
-<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/751592/7707372/css/fonts.css" />
 <meta property="og:image" content="psalm_preview.png" />
 <link rel="stylesheet" href="/assets/css/site.css?21">
 <link rel="icon" type="image/png" href="/favicon.png">
