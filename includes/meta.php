a1604fd4d9daf8ca0148cea79a7ef35b060de924
Matthew Brown <github@muglug.com>
2022-04-11 11:44:25 -0400

    Bump version

--- includes/meta.php
+++ includes/meta.php
@@ -1,5 +1,5 @@
 <script src="/assets/js/codemirror.js?1"></script>
 <meta property="og:image" content="psalm_preview.png" />
-<link rel="stylesheet" href="/assets/css/site.css?21">
+<link rel="stylesheet" href="/assets/css/site.css?22">
 <link rel="icon" type="image/png" href="/favicon.png">
 <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
