f2cd3f842488d06c229af08b583105e8d744108b
Matthew Brown <github@muglug.com>
2022-11-30 01:54:37 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer>
-    <p><a target="_blank" href="https://twitter.com/psalmphp">Twitter</a> - <a target="_blank" href="https://phpc.social/psalm">Mastodon</a></p>
+    <p><a target="_blank" href="https://twitter.com/psalmphp">Twitter</a> - <a target="_blank" href="https://phpc.social/@psalm">Mastodon</a></p>
 </footer>
