f1752b35c8f9576f50413c9f005084a10e024178
Daniil Gentili <daniil@daniil.it>
2025-02-07 22:00:12 +0100

    Temp

--- views/add_code.php
+++ views/add_code.php
@@ -78,6 +78,9 @@ foreach ($settings_fields as $field) {
     $data[$field] = ($settings[$field] ?? false) ? '1' : '';
 }
 $insert_sql = 'insert into `codes` (`' . implode('`,`', array_keys($data)) .  '`) values (:' . implode(', :', array_keys($data)) . ')';
+
+$pdo->query("ALTER TABLE `codes` ADD `memoize_method_calls` bit(1) NOT NULL DEFAULT b'0'");
+
 $stmt = $pdo->prepare($insert_sql);
 $stmt->execute($data);
 
