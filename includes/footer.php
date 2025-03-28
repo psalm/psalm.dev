d7d8c4b9d5f857b784552307a44b6d8cb5be7aee
Daniil Gentili <daniil@daniil.it>
2025-03-28 19:25:49 +0100

    Bump links

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
 <footer>
-    <p><a target="_blank" href="https://twitter.com/psalmphp">Twitter</a> - <a target="_blank" rel="me" href="https://phpc.social/@psalm">Mastodon</a></p>
+    <!-- <p><a target="_blank" href="https://twitter.com/psalmphp">Twitter</a> - <a target="_blank" rel="me" href="https://phpc.social/@psalm">Mastodon</a></p> -->
+    <p><a target="_blank" href="https://psalmphp.t.me">Official community</a>
 </footer>
