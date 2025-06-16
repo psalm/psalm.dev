d9945a6a84cdcaec0ddf93f481712d8f3d6bbe90
Matthew Brown <github@muglug.com>
2025-06-15 22:04:49 -0400

    Fix formatting

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
-    <p>Follow Psalm news: <a target="_blank" href="https://t.me/psalmphp">Telegram</a> - <a target="_blank" rel="me" href="https://phpc.social/@psalm">Mastodon</a> — <a target="_blank" href="https://x.com/psalmphp">X</a></p>
-    <p><a target="_blank" href="https://psalmphp.t.me">Official community on Telegram</a>
+    <p>Follow Psalm news: <a target="_blank" href="https://t.me/psalmphp">Telegram</a> — <a target="_blank" rel="me" href="https://phpc.social/@psalm">Mastodon</a> — <a target="_blank" href="https://x.com/psalmphp">X</a></p>
+    <p><a target="_blank" href="https://psalmphp.t.me">Official community</a> (Telegram)</p>
 </footer>
