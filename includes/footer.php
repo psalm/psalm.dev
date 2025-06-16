c537bf2ef08a06d9017e62648177b92f0f93e8fc
Matthew Brown <github@muglug.com>
2025-06-15 22:03:17 -0400

    Add back social links

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
-    <!-- <p><a target="_blank" href="https://twitter.com/psalmphp">Twitter</a> - <a target="_blank" rel="me" href="https://phpc.social/@psalm">Mastodon</a></p> -->
-    <p><a target="_blank" href="https://psalmphp.t.me">Official community</a>
+    <p>Follow Psalm news: <a target="_blank" href="https://t.me/psalmphp">Telegram</a> - <a target="_blank" rel="me" href="https://phpc.social/@psalm">Mastodon</a> — <a target="_blank" href="https://x.com/psalmphp">X</a></p>
+    <p><a target="_blank" href="https://psalmphp.t.me">Official community on Telegram</a>
 </footer>
